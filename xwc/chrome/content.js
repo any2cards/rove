5b23c4092ba332c6e40135f24be054f69e372b26
any2cards <any2cards@yahoo.com>
2025-05-21 16:50:18 -0700

    Update content.js

--- xwc/chrome/content.js
+++ xwc/chrome/content.js
@@ -9,7 +9,6 @@ var expansion_conversion = {
 var expansion_card_type = {}
 
 const repoBaseUrl = 'https://raw.githubusercontent.com/any2cards/rove/master';
-// const repoBaseUrl = 'http://localhost';
 const imgUrl = `${repoBaseUrl}/images/`;
 const dataUrl = `${repoBaseUrl}/data/`;
 
