c629dbb598f45839bfd8915dd83382a769fac271
any2cards <any2cards@yahoo.com>
2025-05-25 17:17:49 -0700

    Fix Invisible Hand Trait Exoskeletal Development

---
+++ data/traits.js
@@ -0,0 +1,170 @@
+[
+  {
+    "name": "aster trait back",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-aster-trait-back.png",
+    "xws": "astertraitback",
+    "cardno": "c-###"
+  },
+  {
+    "name": "plasma approximation",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-plasma-approximation.png",
+    "xws": "plasmaapproximation",
+    "cardno": "c-111"
+  },
+  {
+    "name": "c-111",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-plasma-approximation.png",
+    "xws": "plasmaapproximation",
+    "cardno": "c-111"
+  },
+  {
+    "name": "solar particle beam",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-solar-particle-beam.png",
+    "xws": "solarparticlebeam",
+    "cardno": "c-112"
+  },
+  {
+    "name": "c-112",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-solar-particle-beam.png",
+    "xws": "solarparticlebeam",
+    "cardno": "c-112"
+  },
+  {
+    "name": "stellar forging",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-stellar-forging.png",
+    "xws": "stellarforging",
+    "cardno": "c-110"
+  },
+  {
+    "name": "c-110",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/aster/rv-stellar-forging.png",
+    "xws": "stellarforging",
+    "cardno": "c-110"
+  },
+  {
+    "name": "blazing charge",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-blazing-charge.png",
+    "xws": "blazingcharge",
+    "cardno": "c-105"
+  },
+  {
+    "name": "c-105",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-blazing-charge.png",
+    "xws": "blazingcharge",
+    "cardno": "c-105"
+  },
+  {
+    "name": "canyon temper trait back",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-canyon-temper-trait-back.png",
+    "xws": "canyontempertraitback",
+    "cardno": "c-###"
+  },
+  {
+    "name": "earthen whisper",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-earthen-whisper.png",
+    "xws": "earthenwhisper",
+    "cardno": "c-104"
+  },
+  {
+    "name": "c-104",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-earthen-whisper.png",
+    "xws": "earthenwhisper",
+    "cardno": "c-104"
+  },
+  {
+    "name": "streak boarding",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-streak-boarding.png",
+    "xws": "streakboarding",
+    "cardno": "c-106"
+  },
+  {
+    "name": "c-106",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/canyon-temper/rv-streak-boarding.png",
+    "xws": "streakboarding",
+    "cardno": "c-106"
+  },
+  {
+    "name": "conceptualist trait back",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-conceptualist-trait-back.png",
+    "xws": "conceptualisttraitback",
+    "cardno": "c-###"
+  },
+  {
+    "name": "ethos rhetoric",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-ethos-rhetoric.png",
+    "xws": "ethosrhetoric",
+    "cardno": "c-121"
+  },
+  {
+    "name": "c-121",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-ethos-rhetoric.png",
+    "xws": "ethosrhetoric",
+    "cardno": "c-121"
+  },
+  {
+    "name": "kanon inventio",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-kanon-inventio.png",
+    "xws": "kanoninventio",
+    "cardno": "c-120"
+  },
+  {
+    "name": "c-120",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-kanon-inventio.png",
+    "xws": "kanoninventio",
+    "cardno": "c-120"
+  },
+  {
+    "name": "pathos rhetoric",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-pathos-rhetoric.png",
+    "xws": "pathosrhetoric",
+    "cardno": "c-119"
+  },
+  {
+    "name": "c-119",
+    "points": 0,
+    "expansion": "Rove",
+    "image": "traits/rove/core/conceptualist/rv-pathos-rhetoric.png",
+    "xws": "pathosrhetoric",
+    "cardno": "c-119"
+  },
+]
